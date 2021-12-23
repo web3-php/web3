305f0ea87d1e7efbf9a7bed103f58cab781a3736
Hammer Rot <90797681+hammer-rot@users.noreply.github.com>
2021-12-24 00:38:09 +0100

    improvement: improve web3_sha3 is string hex

--- tests/Namespaces/Web3.php
+++ tests/Namespaces/Web3.php
@@ -24,5 +24,12 @@ test('sha3', function () {
     )->once()->andReturn('0x5b2c76da96136d193336fad3fbc049867b8ca157da22f69ae0e4923648250acc');
 
     expect($this->web3->sha3('hello world'))
-        ->toBe('0x5b2c76da96136d193336fad3fbc049867b8ca157da22f69ae0e4923648250acc');
+    ->toBe('0x5b2c76da96136d193336fad3fbc049867b8ca157da22f69ae0e4923648250acc');
+
+    $this->transporter->shouldReceive('request')->with(
+        'web3_sha3', ['0x30782068656c6c6f20776f726c64']
+    )->once()->andReturn('0x9b886e3c06058fc79e473463c5a3a43d9bead0ff9c8ecf5858840094a7ace88e');
+
+    expect($this->web3->sha3('0x hello world'))
+            ->toBe('0x9b886e3c06058fc79e473463c5a3a43d9bead0ff9c8ecf5858840094a7ace88e');
 });

555a2c03dc2cf9191dc9f77570d8e32ab2153f8f
Yudi Kubota <yudikubota@gmail.com>
2022-03-30 10:31:06 -0300

    Implement eth_getLogs method

--- src/Namespaces/Eth.php
+++ src/Namespaces/Eth.php
@@ -10,6 +10,7 @@ use Web3\Exceptions\TransporterException;
 use Web3\Formatters\BigIntegerToHex;
 use Web3\Formatters\HexToBigInteger;
 use Web3\Formatters\HexToWei;
+use Web3\Validators\FilterObject;
 use Web3\ValueObjects\Transaction;
 use Web3\ValueObjects\Wei;
 
@@ -235,6 +236,32 @@ final class Eth
         assert(is_string($result));
 
         return HexToBigInteger::format($result);
+    }
+
+    /**
+     * Returns an array of all logs matching a given filter object.
+     *
+     * @see https://docs.infura.io/infura/networks/ethereum/json-rpc-methods/eth_getlogs
+     * @throws ErrorException|TransporterException
+     * @return array<>
+     */
+    public function getLogs($filterObject = []): array
+    {
+        FilterObject::validate($filterObject);
+
+        $result = $this->transporter->request('eth_getLogs', $filterObject);
+
+        /** @var array<string, string|array<string>> $result */
+        assert(is_array($result));
+
+        /** @var array<string, string> $log */
+        foreach ($result as $logKey => $log) {
+            foreach (['blockNumber', 'logIndex', 'transactionIndex'] as $key) {
+                $result[$logKey][$key] = HexToBigInteger::format($log[$key]);
+            }
+        }
+
+        return $result;
     }
 
     /**
